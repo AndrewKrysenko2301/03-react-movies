cee7fc6feb1a411eac38cfdef95718b91225ce6b
Andrew_Krysenko <krysenko92@gmail.com>
2025-09-08 18:52:33 +0200

    react-movies-finish1

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,33 +1,60 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.module.css";
+import toast, { Toaster } from "react-hot-toast";
+import SearchBar from "../SearchBar/SearchBar";
+import MovieGrid from "../MovieGrid/MovieGrid";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import MovieModal from "../MovieModal/MovieModal";
+import { fetchMovies } from "../../services/movieService";
+import type { Movie } from "../../types/movie";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+
+  const handleSearch = async (query: string) => {
+    try {
+      setLoading(true);
+      setError(false);
+      setMovies([]);
+
+      const data = await fetchMovies(`search/movie?query=${query}`);
+      if (data.results.length === 0) {
+        toast.error("No movies found for your request.");
+        setMovies([]);
+        return;
+      }
+      setMovies(data.results);
+    } catch {
+      setError(true);
+      toast.error("Something went wrong. Try again later.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleSelectMovie = (movie: Movie) => {
+    setSelectedMovie(movie);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedMovie(null);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <SearchBar onSubmit={handleSearch} />
+      <Toaster position="top-right" />
+      {loading && <Loader />}
+      {error && <ErrorMessage />}
+      {!loading && !error && (
+        <MovieGrid movies={movies} onSelect={handleSelectMovie} />
+      )}
+      {selectedMovie && (
+        <MovieModal movie={selectedMovie} onClose={handleCloseModal} />
+      )}
     </>
   );
 }
--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,10 +1,8 @@
-// src/services/movieService.ts
 import axios from "axios";
 import type { AxiosResponse } from "axios";
 
 import type { Movie } from "../types/movie";
 
-// Типизация ответа от TMDB
 export interface MoviesResponse {
   page: number;
   results: Movie[];
@@ -12,7 +10,6 @@ export interface MoviesResponse {
   total_results: number;
 }
 
-// Функция для получения фильмов
 export const fetchMovies = async (
   endpoint: string
 ): Promise<MoviesResponse> => {
@@ -21,7 +18,7 @@ export const fetchMovies = async (
     {
       headers: {
         Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZmZjNGYwMjVhYWJkMGY2YjE2MzgwYzlmZDdjODJiYSIsInN1YiI6IjY4YmM5ZTNjZTFjODBkMTE1NDk0NzBjZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hlTBp6JfFTPU3J7FXh9rG_Z7vpiDCBfMixVtS_b5Occ",
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZmZjNGYwMjVhYWJkMGY2YjE2MzgwYzlmZDdjODJiYSIsIm5iZiI6MTc1NzE5MTc0MC44MjIsInN1YiI6IjY4YmM5ZTNjZTFjODBkMTE1NDk0NzBjZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hlTBp6JfFTPU3J7FXh9rG_Z7vpiDCBfMixVtS_b5Occ",
         "Content-Type": "application/json",
       },
     }
